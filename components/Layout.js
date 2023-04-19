ca10c9d2eade1285f6db0a112a1b337020bc5062
isuRuSajit <89340506+isuRuSajit@users.noreply.github.com>
2023-04-19 18:21:21 +0530

    dashboard

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/Nav";
+
+export default function Layout({ children }) {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          Not signed in <br />
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white text-black font-bold rounded-lg p-2 px-4"
+          >
+            Sign in Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="bg-blue-900 min-h-screen flex text-black">
+      <Nav />
+      <div className="flex-grow bg-white mt-2 mr-2 rounded-lg p-3">
+        {children}
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,25 @@
-import Google from "next-auth/providers/google";
+/* eslint-disable @next/next/no-img-element */
 import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/Nav";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          Not signed in <br />
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white text-black font-bold rounded-lg p-2 px-4"
-          >
-            Sign in Google
-          </button>{" "}
+  if (!session) return;
+  return (
+    <Layout>
+      <div className="text-blue-950 flex justify-between">
+        <h1>
+          {" "}
+          Hello , <b>{session?.user?.name}</b>
+        </h1>
+
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          {" "}
+          <img src={session?.user?.image} alt="d" className="w-6 h-6" />
+          <span className="px-2 font-bold"> {session?.user?.name}</span>
         </div>
       </div>
-    );
-  }
-  return (
-    <div className="bg-blue min-h-full">
-      <div>logged in {session.user.email}</div>
-    </div>
+    </Layout>
   );
 }
