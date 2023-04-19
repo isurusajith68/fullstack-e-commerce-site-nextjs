d54a41d1bca9be79168fd4673acf529aaa253210
isuRuSajit <89340506+isuRuSajit@users.noreply.github.com>
2023-04-19 12:02:00 +0530

    mongodb auth

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,26 @@
+import Google from "next-auth/providers/google";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (session) {
+  if (!session) {
     return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          Signed in as {session.user.email} <br />
-          <button
-            className="bg-white text-black font-bold rounded-lg p-2 px-4"
-            onClick={() => signOut()}
-          >
-            Sign Out
-          </button>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <>
       <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
           Not signed in <br />
           <button
-            onClick={() => signIn()}
+            onClick={() => signIn("google")}
             className="bg-white text-black font-bold rounded-lg p-2 px-4"
           >
             Sign in Google
           </button>{" "}
         </div>
       </div>
-    </>
+    );
+  }
+  return (
+    <div className="bg-blue min-h-full">
+      <div>logged in {session.user.email}</div>
+    </div>
   );
 }
