159e99ac06bd8e5d446ddec31761ff43fc36eeed
isuRuSajit <89340506+isurusajith68@users.noreply.github.com>
2023-05-09 18:03:20 +0530

    cc

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,13 +3,13 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 export const authOptions = {
-  // Configure one or more authentication providers
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
+
   ],
   adapter: MongoDBAdapter(clientPromise),
 };
